7084052ee46572d68f0a9179e7040040b8e08fac
Jiri Otoupal <jiri-otoupal@ips-database.eu>
2025-12-07 14:08:47 +0100

    initial

--- tricoder/__init__.py
+++ tricoder/__init__.py
@@ -10,7 +10,7 @@ from .train import train_model
 try:
     from .__about__ import __version__, __author__, __email__, __license__, __copyright__
 except ImportError:
-    __version__ = "1.1.0"
+    __version__ = "1.1.1"
     __author__ = "Jiri Otoupal"
     __email__ = "j.f.otoupal@gmail.com"
     __license__ = "Non-Commercial License"
